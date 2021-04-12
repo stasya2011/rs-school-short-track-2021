fc1ba1d75d9ea5e5dd919ccc0c13113e551ac0f9
Stasya Geleisha <s.geleisha@gmail.com>
2021-04-12 16:04:09 +0300

    feat: do task_4

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,54 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const arr = matrix.map((element, index) => {
+    const s = element.map((itemInner, indexInner) => {
+      let count = 0;
+
+      if (matrix[index][indexInner + 1] === true
+         && indexInner + 1 !== undefined) {
+        count += 1;
+      }
+      if (matrix[index][indexInner - 1] === true
+         && indexInner - 1 !== undefined) {
+        count += 1;
+      }
+
+      if (matrix[index - 1] !== undefined
+         && matrix[index - 1][indexInner] === true) {
+        count += 1;
+      }
+
+      if (matrix[index + 1] !== undefined && matrix[index + 1][indexInner] === true) {
+        count += 1;
+      }
+
+      if (matrix[index - 1] !== undefined
+         && indexInner - 1 !== undefined && matrix[index - 1][indexInner - 1] === true) {
+        count += 1;
+      }
+
+      if (matrix[index - 1] !== undefined
+         && indexInner + 1 !== undefined && matrix[index - 1][indexInner + 1] === true) {
+        count += 1;
+      }
+
+      if (matrix[index + 1] !== undefined
+         && indexInner + 1 !== undefined && matrix[index + 1][indexInner + 1] === true) {
+        count += 1;
+      }
+
+      if (matrix[index + 1] !== undefined && indexInner - 1 !== undefined
+         && matrix[index + 1][indexInner - 1] === true) {
+        count += 1;
+      }
+      return count;
+    });
+
+    return s;
+  });
+  return arr;
 }
 
 module.exports = minesweeper;
